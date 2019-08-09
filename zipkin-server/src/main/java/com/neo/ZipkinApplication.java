2a9879bf5267515fa84c10a4bac2ef3f437ba03b
chenyan <chenyan2014@163.com>
2019-08-09 17:11:58 +0800

    整合修改为mysql

--- zipkin-server/src/main/java/com/neo/ZipkinApplication.java
+++ zipkin-server/src/main/java/com/neo/ZipkinApplication.java
@@ -3,7 +3,11 @@ package com.neo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import zipkin.server.EnableZipkinServer;
+import zipkin.storage.mysql.MySQLStorage;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -13,5 +17,8 @@ public class ZipkinApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZipkinApplication.class, args);
 	}
-
+	@Bean
+	public MySQLStorage mySQLStorage(DataSource datasource) {
+		return MySQLStorage.builder().datasource(datasource).executor(Runnable::run).build();
+	}
 }
